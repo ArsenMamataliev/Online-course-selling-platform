1c3eba768c99ec48336a8542e9b4445fbd051bd9
arsenmamataliev <a.mamataliev93@gmail.com>
2021-12-13 01:12:27 +0600

    Added some amazon photos to photoSlider

--- src/components/PhotoSlider.js
+++ src/components/PhotoSlider.js
@@ -2,12 +2,15 @@ import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
-import amazon from '../media/amazonlogo.jpg';
-import amazon1 from '../media/amazoncenter.jpg';
-import amazon2 from '../media/amazoncenter2.jpg';
-import amazon3 from '../media/amazonlogo2.jpg';
-import amazon4 from '../media/amazonwarehouse.jpg';
-import amazon5 from '../media/amazonlogomini.jpg';
+import amazon from '../media/amazoncenter.jpg';
+import amazon1 from '../media/amazoncenter2.jpg';
+import amazon2 from '../media/amazoncenter3.jpg';
+import amazon3 from '../media/amazone-office.png';
+import amazon4 from '../media/amazone-office2.jpg';
+import amazon5 from '../media/amazonlogo.jpg';
+import amazon6 from '../media/amazonlogo2.jpg';
+import amazon7 from '../media/amazonwarehouse.jpg';
+import amazon8 from '../media/amazonwarehouse2.jpeg';
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
@@ -24,6 +27,9 @@ function PhotoSlider() {
       <div data-src={amazon3} />
       <div data-src={amazon4} />
       <div data-src={amazon5} />
+      <div data-src={amazon6} />
+      <div data-src={amazon7} />
+      <div data-src={amazon8} />
     </AutoplaySlider>
   )
 }
--- src/components/VideoLesson.js
+++ src/components/VideoLesson.js
@@ -71,3 +71,4 @@ export default function VideoLesson({name, subtopics}) {
     </div>
   );
 }
+
